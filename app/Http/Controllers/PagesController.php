c6972f5b7474aa3a990e27b7d759c792fd54966f
fahmirosyadi <m.fahmi.r26@gmail.com>
2020-09-25 10:19:00 +0700

    Memperbaiki email

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -138,7 +138,7 @@ class PagesController extends Controller
     }
 
     public function contactProcess(Request $request) {
-
+        return env('MAIL_FROM_ADDRESS');
         $request->validate([
             'name' => 'required',
             'email' => 'email',
